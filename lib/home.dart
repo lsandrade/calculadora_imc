29d27fb4ae649fbe75992952fadc61c117025464
Luan Andrade <luansandrade92@hotmail.com>
2021-04-06 22:45:20 -0300

    create SingleChildScrollView and ElevatedButton

--- lib/home.dart
+++ lib/home.dart
@@ -22,33 +22,57 @@ class _HomeState extends State<Home> {
         ],
       ),
       backgroundColor: Colors.white,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Icon(
-            Icons.person_outline,
-            size: 120.0,
-            color: Colors.blueAccent,
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Peso (kg)",
-              labelStyle: TextStyle(color: Colors.blueAccent),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10.0, 0.0, 10.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Icon(
+              Icons.person_outline,
+              size: 120.0,
+              color: Colors.blueAccent,
             ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.blueAccent, fontSize: 24.0),
-          ),
-          TextField(
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(
-              labelText: "Altura (cm)",
-              labelStyle: TextStyle(color: Colors.blueAccent),
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Peso (kg)",
+                labelStyle: TextStyle(color: Colors.blueAccent),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.blueAccent, fontSize: 24.0),
             ),
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.blueAccent, fontSize: 24.0),
-          )
-        ],
+            TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: "Altura (cm)",
+                labelStyle: TextStyle(color: Colors.blueAccent),
+              ),
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.blueAccent, fontSize: 24.0),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10.0, bottom: 10.0),
+              child: Container(
+                height: 50.0,
+                child: ElevatedButton(
+                  onPressed: () => null,
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                      primary: Colors.blueAccent,
+                      textStyle: TextStyle(fontSize: 24.0)),
+                ),
+              ),
+            ),
+            Text(
+              "Info",
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.blueAccent, fontSize: 24.0),
+            )
+          ],
+        ),
       ),
     );
   }
