6a135fdf1e33927035d718a0c7ae0ec3a8e93a53
Luan Andrade <luansandrade92@hotmail.com>
2021-04-06 22:12:43 -0300

    create basic scaffold widget for home

---
+++ lib/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Calculadora de IMC"),
+        centerTitle: true,
+        backgroundColor: Colors.blueAccent,
+        actions: [IconButton(icon: Icon(Icons.refresh), onPressed: null)],
+      ),
+    );
+  }
+}
