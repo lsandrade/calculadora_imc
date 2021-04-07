e15ec7e1dee95fb6a7bcf39add8e5eac8baf12c6
Luan Andrade <luansandrade92@hotmail.com>
2021-04-06 22:19:17 -0300

    add icon and textfields

--- lib/home.dart
+++ lib/home.dart
@@ -13,7 +13,42 @@ class _HomeState extends State<Home> {
         title: Text("Calculadora de IMC"),
         centerTitle: true,
         backgroundColor: Colors.blueAccent,
-        actions: [IconButton(icon: Icon(Icons.refresh), onPressed: null)],
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: null,
+            color: Colors.white,
+          )
+        ],
+      ),
+      backgroundColor: Colors.white,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Icon(
+            Icons.person_outline,
+            size: 120.0,
+            color: Colors.blueAccent,
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Peso (kg)",
+              labelStyle: TextStyle(color: Colors.blueAccent),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.blueAccent, fontSize: 24.0),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              labelText: "Altura (cm)",
+              labelStyle: TextStyle(color: Colors.blueAccent),
+            ),
+            textAlign: TextAlign.center,
+            style: TextStyle(color: Colors.blueAccent, fontSize: 24.0),
+          )
+        ],
       ),
     );
   }
