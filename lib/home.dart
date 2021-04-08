1f75610ada028d088b687c8c66c3b0aa90ec707d
Luan Andrade <luansandrade92@hotmail.com>
2021-04-07 21:11:41 -0300

    reset form key when refresh

--- lib/home.dart
+++ lib/home.dart
@@ -18,6 +18,7 @@ class _HomeState extends State<Home> {
       weightController.text = "";
       heightController.text = "";
       _infoText = "Informe seus dados";
+      _formKey = GlobalKey<FormState>();
     });
   }
 
